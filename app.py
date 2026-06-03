200d57d000219bf6b404af51f0d13f1474833638
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2026-06-02 23:55:44 -0300

    Tela de login e Dashboard

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 import streamlit as st
 from config import nome_programa, versao_programa
+from database.db import buscar_usuario_por_email
 
 st.set_page_config(
     page_title="ReqFlow 1.0.0",
     page_icon="🎯",
-
 )
 
-st.title(f"Bem Vindo ao {nome_programa}")
-st.markdown(f"""`versão: {versao_programa}`""" )
+if "usuario_logado" not in st.session_state:
+    st.session_state.usuario_logado = None
+#session_state!!
 
-email = st.text_input("Email")
-senha = st.text_input("Senha", type="password")
-if st.button("Entrar"):  
-    st.success(f"Bem-vindo! Você digitou o e-mail: {email} e a senha {senha}")
+
+def tela_login():
+    st.title(f"Bem-vindo ao {nome_programa}")
+    st.markdown(f"`versão: {versao_programa}`")
+
+    email = st.text_input("E-mail")
+    senha = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+        usuario = buscar_usuario_por_email(email)
+
+        if usuario is None:
+            st.error("E-mail não encontrado.")
+        elif not usuario.status:
+            st.error("Usuário inativo. Contate o administrador.")
+        elif usuario.senha != senha:
+            st.error("Senha incorreta.")
+        else:
+            st.session_state.usuario_logado = usuario
+            st.rerun()  
+
+def tela_home(usuario):
+    st.title(f"Olá, {usuario.nome}!")
+    st.markdown(f"**{usuario.cargo}**.")
+    st.info("Dashboard")
+
+    if st.button("Sair"):
+        st.session_state.usuario_logado = None
+        st.rerun()
+
+if st.session_state.usuario_logado is None:
+    tela_login()
+else:
+    tela_home(st.session_state.usuario_logado)
---
+++ database/db.py
@@ -0,0 +1,17 @@
+from models.usuario import Usuario
+
+_usuarios: list[Usuario] = [
+    Usuario(id=1, nome="Raquel",     email="raquel@reqflow.com",  senha="1234", cargo="analista",     status=True),
+    Usuario(id=2, nome="Nicole",     email="nicole@reqflow.com",  senha="1234", cargo="gerente",      status=True),
+    Usuario(id=3, nome="Marina",     email="marina@reqflow.com",  senha="1234", cargo="cliente",      status=True),
+    Usuario(id=4, nome="Thiago",      email="Thiago@reqflow.com",   senha="1234", cargo="desenvolvedor",status=True),
+    Usuario(id=5, nome="Eduarda",   email="eduarda@reqflow.com",senha="1234", cargo="testador",     status=True),
+    Usuario(id=6, nome="Matehus",    email="matheus@reqflow.com", senha="1234", cargo="analista",     status=False),
+]
+
+
+def buscar_usuario_por_email(email: str) -> Usuario | None:
+    for usuario in _usuarios:
+        if usuario.email == email:
+            return usuario
+    return None
